26f2967fd4d14a57b2d4b17b0e0455fadf2ae600
Ashani Imalsha <62203457+AshaniImalsha@users.noreply.github.com>
2021-09-15 21:34:47 +0530

    Add files via upload

---
+++ views/navbar.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Style/navbar_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header">
+        
+        <button class="header-button" id="btnNav" type="button">
+            <i class="fa fa-bars fa-lg"></i>
+        </button>
+        <img src="../Asserts/logo.jpg" alt="" class="open-img">
+        <label for="" class="date"> <?php 
+                                        $day;
+                                        if(date("d") == 1){
+                                            $day = "st ";
+                                        }elseif(date("d") == 2){
+                                            $day = "nd ";
+                                        }elseif(date("d") == 3){
+                                            $day = "rd ";
+                                        }else{
+                                            $day = "th ";
+                                        }
+
+                                        echo "Today, " . date("d") . $day .date("M") . " " . date("Y") . "<br>"; 
+                                        
+                                    ?>
+        </label>
+        <div class="notification"><a href="#" ><i class="fa fa-bell fa-lg" "></i></a></div>
+        <span class="user-login">UserName</span>
+        <img  class="img-rounded-circle" src="../Asserts/avator.jpg" alt="">
+        
+    </header>
+    <nav class="nav">
+        <div class="nav-links">
+            <a href="#" class="nav-link nav-link-active">
+                <i class="fa fa-user fa-lg"><span>My Profile</span></i>
+            </a>
+            <a href="#" class="nav-link">
+                <i class="fa fa-bars fa-lg"><span>My Profile</span></i>
+            </a>
+            <a href="#" class="nav-link">
+                <i class="fa fa-bars fa-lg"><span>My Profile</span></i>
+            </a>
+        </div> 
+        <div class="nav-overlay"></div>    
+    </nav>
+    <main>
+        
+    </main>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", () =>{
+            const nav = document.querySelector(".nav");
+
+            document.querySelector("#btnNav").addEventListener("click" , () =>{
+                nav.classList.add("nav-open");
+            });
+
+            document.querySelector(".nav-overlay").addEventListener("click" , () =>{
+                nav.classList.remove("nav-open");
+            });
+        });
+    </script>
+</body>
+</html>
