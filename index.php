699d1c4838c041b6fcf58186dc3ef4d72b7e06e4
AshaniImalsha <ashaniimalsha26@gmail.com>
2021-09-23 22:32:31 +0530

    index file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'config/DatabaseConf.php';
+require 'libs/App.php';
+require 'libs/Controller.php';
+require 'libs/Model.php';
+require 'libs/View.php';
+require 'libs/Database.php';
+require 'libs/Session.php';
+
+
+
+$app = new App();
