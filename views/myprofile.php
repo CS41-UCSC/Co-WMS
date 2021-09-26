7cc38454b4be7aed44d51a50cd5e6fec4914f99d
Ashani Imalsha <62203457+AshaniImalsha@users.noreply.github.com>
2021-09-27 01:56:37 +0530

    display data

--- views/myprofile.php
+++ views/myprofile.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Style/myprofile_style.css">
-    <link rel="stylesheet" href="../style/navbar_style.css" type="text/css">
+    <link rel="stylesheet" href="../Co-WMS/style/myprofile_style.css" type="text/css">
+    <link rel="stylesheet" href="../Co-WMS/style/navbar_style.css" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
     <title>Document</title>
@@ -16,7 +16,7 @@
         <button class="header-button" id="btnNav" type="button">
             <i class="fa fa-bars fa-lg"></i>
         </button>
-        <img src="../Asserts/logo.jpg" alt="" class="open-img">
+        <img src="../Co-WMS/Asserts/logo.jpg" alt="" class="open-img">
         <label for="" class="date"> <?php 
                                         $day;
                                         if(date("d") == 1){
@@ -34,8 +34,8 @@
                                     ?>
         </label>
         <div class="notification"><a href="#" ><i class="fa fa-bell fa-lg" "></i></a></div>
-        <span class="user-login">UserName</span>
-        <img  class="img-rounded-circle" src="../Asserts/avator.jpg" alt="">
+        <span class="user-login"><?php echo $_SESSION['login_user'] ?></span>
+        <img  class="img-rounded-circle" src="../Co-WMS/Asserts/avator.jpg" alt="">
         
 
     </header>
@@ -64,30 +64,38 @@
     <main >
         <div class="container">
             
+
+            <?php  $result = $this->users; ?>
+
             <div class="item1"> 
+                <form action="myprofile/savephoto" method="POST" enctype="multipart/form-data" name="form1">
                 <div class="img-card">
-                    <img src="../Asserts/avator.jpg" alt="" id="photo">
-                    <input type="file" id="file">
-                    <label for="file" id="uploadBtn">Choose photo</label>
+                
+                    <img src="../Co-WMS/Asserts/<?php echo $result[0]['Userimg']; ?>" alt="" id="photo">
+                    <input type="file" id="file" name="image">
+                    <div class="btn"><label for="file" id="uploadBtn">Choose photo</label></div>
+                    <input type="submit" value="Save Changes" id="sbtbtn" onclick="submitphoto();">
+
                 </div>
+                </form>
             </div>
             <div class="item2"> 
-                <form action="" class="data-form">
-                    <label for="fname">First Name</label>
-                    <input type="text" name="fname" value="Ashani" readonly class="input-fields"><br>
-                    <label for="lname">Last Name</label>
-                    <input type="text" name="fname" value="Imalsha" readonly class="input-fields"><br>
-                    <label for="dep">Department</label>
-                    <input type="text" name="fname" value="Department" readonly class="input-fields"><br>
-                    <label for="team">Team</label>
-                    <input type="text" name="fname" value="Team" readonly class="input-fields"><br>
-                    <label for="add">Address</label>
-                    <input type="text" name="fname" value="Address" readonly class="input-fields"><br>
+                <form action="myprofile/saveform" method="POST" class="data-form">
+                    <label for="empid">Employee ID</label>
+                    <input type="text" name="empid" value="<?php echo $result[0]['EmpID'] ?>" readonly class="input-fields"><br>
+                    <label for="empname">Name (with initials)</label>
+                    <input type="text" name="empname" value="<?php echo $result[0]['EmpName'] ?>" readonly class="input-fields"><br>
+                    <label for="empemail">Email</label>
+                    <input type="text" name="empemail" value="<?php echo $result[0]['EmpEmail'] ?>" readonly class="input-fields"><br>
+                    <label for="emprole">Employee Role</label>
+                    <input type="text" name="emprole" value="<?php echo $result[0]['EmpRole'] ?>" readonly class="input-fields"><br>
+                    <label for="empstatus">Employee Status</label>
+                    <input type="text" name="empstatus" value="<?php echo $result[0]['EmpStatus'] ?>" readonly class="input-fields"><br>
                     <label for="add">Current Password</label>
-                    <input type="password" name="cpass" value="current password" class="input-fields"><br>
+                    <input type="password" name="cpass" value="<?php echo $result[0]['PASSWORD'] ?>" class="input-fields"><br>
                     <label for="add">New Password</label>
-                    <input type="password" name="npass" value="new password" class="input-fields"><br>
-                    <input type="submit" value="Save Changes" class="button">
+                    <input type="password" name="npass" value="<?php echo $result[0]['PASSWORD'] ?>" class="input-fields"><br>
+                    <input type="submit" value="Save Changes" class="button" >
                 </form>
             </div>
             
@@ -137,6 +145,12 @@
 
     </script>
 
+    <script>
+
+        function submitphoto(){
+            document.form1.action()
+        }
+    </script>
     
 </body>
 </html>
