0e363fe7da9024741465c5026df894113551958e
AshaniImalsha <ashaniimalsha26@gmail.com>
2021-09-30 18:58:33 +0530

    add image

--- views/myprofile.php
+++ views/myprofile.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,11 +8,44 @@
     <link rel="stylesheet" href="../Co-WMS/style/myprofile_style.css" type="text/css">
     <link rel="stylesheet" href="../Co-WMS/style/navbar_style.css" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
     <title>Document</title>
 </head>
 
 <body class="preload">
+
+        <?php  
+
+        $result = $this->users; 
+        //echo $_SESSION['password'];
+
+        if(! empty($_SESSION['photo'])){
+            if($_SESSION['photo']=="yes"){
+                echo '<script>swal("Success!", "Photo added!", "success")</script>';
+                $_SESSION['photo']=null;
+            }
+            else if($_SESSION['photo']=="no") {
+                echo '<script>swal("Failed!", "Try Again!","error")</script>';
+                $_SESSION['photo']=null;
+            }
+                
+        }
+
+        if(! empty($_SESSION['pass'])){
+            if($_SESSION['pass']=="yes"){
+                echo '<script>swal("Success!", "Updated Password!", "success")</script>';
+                $_SESSION['pass']=null;
+            }
+            else if($_SESSION['pass']=="no") {
+                echo '<script>swal("Failed!", "Try Again!","error")</script>';
+                $_SESSION['pass']=null;
+            }
+                
+        }
+    
+        ?>  
+
     <header class="header">
         <button class="header-button" id="btnNav" type="button">
             <i class="fa fa-bars fa-lg"></i>
@@ -35,20 +69,26 @@
         </label>
         <div class="notification"><a href="#" ><i class="fa fa-bell fa-lg" "></i></a></div>
         <span class="user-login"><?php echo $_SESSION['login_user'] ?></span>
-        <img  class="img-rounded-circle" src="../Co-WMS/Asserts/avator.jpg" alt="">
+        <img  class="img-rounded-circle" src="../Co-WMS/Asserts/<?php if($result[0]['Userimg']) {echo $result[0]['Userimg'];} else {echo 'avator.jpg';} ?>" alt="">
         
 
     </header>
+
+    <footer class="footer">
+        <label for="" class="footer-data">© 2021, All rights reserved by CO - WMS <br>
+                        No: 23, Flower Avenue, Colombo 7, Sri Lanka.</label>
+    </footer>
+    
     <nav class="nav">
         <div class="nav-links">
 
-            <a href="#" class="nav-link" >
+            <a href="depManager" class="nav-link" >
                 <i class="fa fa-user fa-lg"><span>Dashboard</span></i>
             </a>
-            <a href="#" class="nav-link nav-link-active">
+            <a href="myprofile" class="nav-link nav-link-active">
                 <i class="fa fa-bars fa-lg"><span>My Profile</span></i>
             </a>
-            <a href="#" class="nav-link">
+            <a href="deptManageTask" class="nav-link">
                 <i class="fa fa-bars fa-lg"><span>My Profile</span></i>
             </a>
             <a href="#" class="nav-link">
@@ -62,25 +102,25 @@
     </nav>
 
     <main >
+
         <div class="container">
             
-
-            <?php  $result = $this->users; ?>
+              
 
             <div class="item1"> 
-                <form action="myprofile/savephoto" method="POST" enctype="multipart/form-data" name="form1">
+                <form  method="POST" enctype="multipart/form-data" name="form1">
                 <div class="img-card">
                 
-                    <img src="../Co-WMS/Asserts/<?php echo $result[0]['Userimg']; ?>" alt="" id="photo">
+                    <img src="../Co-WMS/Asserts/<?php if($result[0]['Userimg']) {echo $result[0]['Userimg'];} else {echo 'avator.jpg';} ?>" alt="" id="photo">
                     <input type="file" id="file" name="image">
                     <div class="btn"><label for="file" id="uploadBtn">Choose photo</label></div>
-                    <input type="submit" value="Save Changes" id="sbtbtn" onclick="submitphoto();">
+                    <input type="submit" value="Save Changes" id="sbtbtn" onclick="checkfile();">
 
                 </div>
                 </form>
             </div>
             <div class="item2"> 
-                <form action="myprofile/saveform" method="POST" class="data-form">
+                <form  method="POST" class="data-form" name="form2">
                     <label for="empid">Employee ID</label>
                     <input type="text" name="empid" value="<?php echo $result[0]['EmpID'] ?>" readonly class="input-fields"><br>
                     <label for="empname">Name (with initials)</label>
@@ -90,12 +130,12 @@
                     <label for="emprole">Employee Role</label>
                     <input type="text" name="emprole" value="<?php echo $result[0]['EmpRole'] ?>" readonly class="input-fields"><br>
                     <label for="empstatus">Employee Status</label>
-                    <input type="text" name="empstatus" value="<?php echo $result[0]['EmpStatus'] ?>" readonly class="input-fields"><br>
+                    <input type="text" name="empstatus" value="<?php echo $result[0]['EmpStatus'] ?>" readonly class="input-fields" ><br>
                     <label for="add">Current Password</label>
-                    <input type="password" name="cpass" value="<?php echo $result[0]['PASSWORD'] ?>" class="input-fields"><br>
+                    <input type="password" name="cpass" id="cpass" value="<?php echo $_SESSION['password'] ?>" class="input-fields" onkeyup="validationc();"><br>
                     <label for="add">New Password</label>
-                    <input type="password" name="npass" value="<?php echo $result[0]['PASSWORD'] ?>" class="input-fields"><br>
-                    <input type="submit" value="Save Changes" class="button" >
+                    <input type="password" name="npass" id="npass" value="" class="input-fields" onkeyup="validationc();"><br>
+                    <input type="submit" value="Save Changes" class="button" onclick="saveform();">
                 </form>
             </div>
             
@@ -103,10 +143,9 @@
         
     </main>
 
-   <footer class="footer">
-        <label for="" class="footer-data">© 2021, All rights reserved by CO - WMS <br>
-                        No: 23, Flower Avenue, Colombo 7, Sri Lanka.</label>
-    </footer>
+    
+
+    
     <script>
         document.addEventListener("DOMContentLoaded", () =>{
             const nav = document.querySelector(".nav");
@@ -123,6 +162,15 @@
             });
         });
 
+        
+
+    </script>
+
+    <script>
+
+        res = null;
+        path = null;
+
         const imgDiv = document.querySelector('.img-card');
         const img = document.querySelector('#photo');
         const file = document.querySelector('#file');
@@ -133,6 +181,7 @@
 
             if (choosedFile){
 
+                path = "yes";
                 const reader = new FileReader();
 
                 reader.addEventListener('load',function(){
@@ -143,13 +192,71 @@
             }
         });
 
-    </script>
+        function checkfile(){
 
-    <script>
+            if(path=="yes")
+                document.form1.action = "myprofile/savephoto";  
 
-        function submitphoto(){
-            document.form1.action()
+            else if (!path){
+
+                swal("Failed!", "Try Again!", "warning");
+                event.preventDefault();
+            }
+
         }
+
+        function validationc(){
+
+            var cpass=document.getElementById("cpass").value;
+
+            var passw=  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+            if(passw.test(cpass))
+            {
+                document.getElementById("cpass").style.border='1px solid green'; 
+                res="green";
+            }
+            else
+            {
+                document.getElementById("cpass").style.border='1px solid red'; 
+                res = "red";
+            }
+
+            
+        }
+
+        function validationn(){
+
+            var npass=document.getElementById("npass").value;
+
+            var passw=  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+            if(passw.test(npass))
+            {
+                document.getElementById("npass").style.border='1px solid green'; 
+                res="green";
+            }
+            else
+            {
+                document.getElementById("npass").style.border='1px solid red'; 
+                res = "red";
+            }
+        }
+
+
+        function saveform(){
+
+            if(res=="green")
+                document.form2.action = "myprofile/saveform";  
+            
+            else if (!res){
+
+                swal("Failed!", "Try Again!", "warning");
+                event.preventDefault();
+            }
+
+        }
+        
     </script>
     
 </body>
