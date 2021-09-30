f64679c3d8ce53c57d03a301b0a12ba7b767907f
AshaniImalsha <ashaniimalsha26@gmail.com>
2021-09-30 20:05:08 +0530

    connect database

--- models/login_Model.php
+++ models/login_Model.php
@@ -18,45 +18,52 @@ class login_Model extends Model{
     function run(){
 
         $user_name=$_POST['user_name'];
-		$password=($_POST['password']);
-		
-		$res= $this->db->select("SELECT * FROM `SystemUser` WHERE `EmpID` = '".$user_name."' AND `password` = '".$password."'");
-		$count = count($res);
-		
-		if ($count > 0) {
-			
-			/*Session::set('role', "user");
-			Session::set('loggedIn', true);
-			Session::set('user_name', $user_name);
-			Session::set('password', $res[0]['password']);*/
-            $_SESSION['login_user'] = $user_name;
-            $_SESSION['num_login_fail']=0;
-            echo $_SESSION['login_user'];
-            $msg = "Hello";
-            $_SESSION['msg'] = $msg ;
-            ob_start();
-			header('location: http://localhost/CO-WMS/depManager');
+		$password=$_POST['password'];
+        
+		$sql = ("SELECT * FROM SystemUser WHERE EmpID='$user_name' ;");
+        $result = $this->db->runQuery($sql);
+        
+        //echo ($result[0][5]);
+
+        if (count($result) > 0) {
             
-		} 
-		   else {
-			/*Session::set('loggedIn', false);*/
-			//header('location: login');
-            $msg = "invalid username and password";
-            $_SESSION['msg'] = $msg ;
-            echo $_SESSION['msg'];
-            $_SESSION['num_login_fail'] ++;
-            $_SESSION['last_login_time'] = time();
-            ob_start();
-            /*$res="no";
-            return $res;*/
-            header('location: http://localhost/CO-WMS/login');
-            /*header("Location: " . $_SERVER["HTTP_REFERER"]);*/
-		}
+            $hash = $result[0][5];
+            
+            $verify = password_verify($password, $hash); 
+
+            if(password_verify($password, $hash)){
+                //echo "hi";
+                $_SESSION['login_user'] = $user_name;
+                $_SESSION['password'] = $password;
+                $_SESSION['num_login_fail']=0;
+                //echo $_SESSION['login_user'];
+                $msg = "Hello";
+                $_SESSION['msg'] = $msg ;
+                ob_start();
+                
+                return true;
+                /*header('location: http://localhost/CO-WMS/depManager');*/
+            }
+            
+        } else {
+
+                $msg = "invalid username and password";
+                $_SESSION['msg'] = $msg ;
+                echo $_SESSION['msg'];
+                $_SESSION['num_login_fail'] ++;
+                $_SESSION['last_login_time'] = time();
+                ob_start();
+                
+                return false;
+                header('location: http://localhost/CO-WMS/login');
+        }
+        
+		
 
     }
 
     function login(){
-        if (isset($_SESSION['num_login_fail'])) {
+       /* if (isset($_SESSION['num_login_fail'])) {
             if ($_SESSION['num_login_fail'] >= 2) {
                 if ((time() - $_SESSION['last_login_time']) < (2 * 60)) {
                     $_SESSION['msg']="try again after 3 minutes.";
@@ -72,9 +79,9 @@ class login_Model extends Model{
             }
         } 
 
-        else{
-            $this->run(); 
-        }   
+        else{*/
+            return $this->run(); 
+        //}   
     }
 
     
