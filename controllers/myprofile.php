7864db4749a8ac3f118407c15c2cd38bdae1975f
AshaniImalsha <ashaniimalsha26@gmail.com>
2021-10-02 23:45:02 +0530

    change save photo logic

--- controllers/myprofile.php
+++ controllers/myprofile.php
@@ -11,14 +11,30 @@ class myprofile extends Controller{
     function index(){
         
         $this->view->users = $this->model->getData($_SESSION['login_user']);
-        
         $this->view->render('myprofile');
 
     }
 
     function saveform(){
 
-        echo $_POST['cpass'];
+        $cpass = $_POST['cpass'];
+        $npass = $_POST['npass'];
+
+        $hash = password_hash($npass, PASSWORD_DEFAULT);
+
+        if($cpass == $npass){
+            $pass = $this->model->setNewPassword($_SESSION['login_user'],$hash); 
+            if($pass == 1)
+                $_SESSION['pass']="yes";
+            else
+                $_SESSION['pass']="no";
+        }
+        else{
+            $_SESSION['pass']="no";
+        }
+
+        $this->view->users = $this->model->getData($_SESSION['login_user']);
+        header('location: http://localhost/CO-WMS/myprofile');
 
     }
 
@@ -31,15 +47,16 @@ class myprofile extends Controller{
         $this->model->setDataPhoto($_SESSION['login_user'],$image_name);
 
         if (move_uploaded_file($tempname, $folder))  {
-            $_SESSION['msg']="1";
-            $this->view->render('myprofile');
+            $_SESSION['photo']="yes";
             
         }else{
-            $this->view->render('myprofile');
+            $_SESSION['photo']="no";
             
-      }
+        }
 
-        /*$this->view->render('myprofile');*/
+        $this->view->users = $this->model->getData($_SESSION['login_user']);
+        header('location: http://localhost/CO-WMS/myprofile');
+        
     }
 
 }
