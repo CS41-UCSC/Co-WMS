0b8c898cdf779c836a0f45591a14dc38e517ea65
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-10-09 23:21:00 +0530

    manageEmployee Controller file

---
+++ controllers/manageEmployee.php
@@ -0,0 +1,21 @@
+<?php
+
+class manageEmployee extends Controller{
+
+    public $msg;
+
+    function __construct()
+    {
+        parent::__construct();
+        session_start();
+    }
+
+    function index(){
+        
+        $this->view->users =  $this->model->getData();
+        $this->view->render('manageEmployee');
+
+
+    }
+    
+}
