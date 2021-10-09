a80542e336f0ebcf0d09a83eeede2e40dc49d870
AshaniImalsha <ashaniimalsha26@gmail.com>
2021-10-10 00:19:23 +0530

    update

--- controllers/login.php
+++ controllers/login.php
@@ -20,8 +20,15 @@ class login extends Controller{
         
         $res = $this->model->login();
         
+        echo $res;
         
-        if($res){
+        if($res=="Team_Member"){
+            header('location: http://localhost/CO-WMS/teamMember');
+        }
+        else if($res=="Team_Leader"){
+            header('location: http://localhost/CO-WMS/teamLeader');
+        }
+        else if($res == "Dept_Manager"){
             header('location: http://localhost/CO-WMS/depManager');
         }
         else if(!$res){
