a7cc5087e836301f8c1919046ab566da26e22c73
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-10-10 09:08:09 +0530

    manageTeamAdd Controller file

---
+++ controllers/manageTeamAdd.php
@@ -0,0 +1,21 @@
+<?php
+
+class manageTeamAdd extends Controller{
+
+    public $msg;
+
+    function __construct()
+    {
+        parent::__construct();
+        session_start();
+    }
+
+    function index(){
+        
+        // $this->view->users =  $this->model->getData();
+        $this->view->render('manageTeamAdd');
+
+
+    }
+    
+}
