0f1c4e37f88d5c1696b5f5557b6429e63f0fd606
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-10-10 09:15:02 +0530

    manageTeamAdd_Model

---
+++ models/manageTeamAdd_Model.php
@@ -0,0 +1,16 @@
+<?php
+
+class manageTeamAdd_Model extends Model{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function getData(){
+
+        $sql = "SELECT * FROM team";
+        return $this->db->runQuery($sql);
+
+    }
+}
