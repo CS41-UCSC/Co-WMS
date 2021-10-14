66c87c34deb4d271ce267f2db2ee883dbc1d5fd0
Ashani Imalsha <62203457+AshaniImalsha@users.noreply.github.com>
2021-10-15 03:25:40 +0530

    Add files via upload

--- controllers/manageEmployeeAdd.php
+++ controllers/manageEmployeeAdd.php
@@ -23,12 +23,47 @@ class manageEmployeeAdd extends Controller{
         $empemail = $_POST['email'];
         $emprole = $_POST['role'];
         $password = $_POST['password'];
-        $empstatus = $_POST['active'];
+        $Cpassword = $_POST['Cpassword'];
+        $empstatus = "";
 
-        $hash = password_hash($password, PASSWORD_DEFAULT);
+        if (isset($_POST['click'])) {
 
-        $this->model->insertEmployee($empid,$empname,$empemail,$emprole,$hash,$empstatus);
+            $empstatus =  $_POST['click'];
+        }
 
+       if($password == $Cpassword){
+
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            $res = $this->model->insertEmployee($empid,$empname,$empemail,$emprole,$hash,$empstatus);
+
+            if($res){
+                $to = '$empemail';
+                $mail_subject = 'Message from Co-WMS website';
+                $email_body = "Hello {$empname},Welcome to the Co-WMS Company \n" ;
+                $email_body .= "Your User Id : {$empid} \n";
+                $email_body .= "Your Password : {$password} \n";
+                $email_body .= "Thank you.";
+                $from = 'From: cowmsofficial@gmail.com';
+                
+                $send_mail_result = mail($empemail, $mail_subject, $email_body, $from);
+
+                if($send_mail_result){
+                    $_SESSION['add-emp-msg'] = "New record created successfully and Email sent";
+                }else{
+                    $_SESSION['add-emp-msg'] = "New record created successfully and Email was not sent";
+                }
+
+            }else{
+                $_SESSION['add-emp-msg'] = "Fialed to add New Employee";
+            }
+
+        }
+        
+        else{
+            $_SESSION['add-emp-msg'] = "Fialed to add New Employee";
+        }
+        
         header('location: http://localhost/CO-WMS/manageEmployeeAdd');
 
     }
