d802a7f310e8e75ee183cbf21ec5e11af411a218
Ashani Imalsha <62203457+AshaniImalsha@users.noreply.github.com>
2021-10-17 01:47:47 +0530

    connected to database

--- views/navigation.js
+++ views/navigation.js
@@ -1,54 +1,77 @@
-var MemberAccess = true;
-var MyProfile = true;
-var ManagerAccess = false;
-var LeaderAccess = false;
-var HRAccess = false;
-var AdminAccess = false;
+function setbutton(v1,v2,v3,v4,v5,v6){
 
-function setbutton(){
+
+	var MemberAccess = v1;
+	var MyProfile = v2;
+	var ManagerAccess = v3;
+	var LeaderAccess = v4;
+	var HRAccess = v5;
+	var AdminAccess = v6;
 	
-	if(!MemberAccess){
+	f1(MemberAccess)
+	f2(MyProfile)
+	f3(ManagerAccess)
+	f4(LeaderAccess)
+	f5(HRAccess)
+	f6(AdminAccess)
+	
+}
+
+function f1(MemberAccess){
+	if(MemberAccess==0){
 		
 		//none display will remove the div
 		document.getElementById("dashboard").style.display="none"; 
 		document.getElementById("my_progress").style.display="none";
-		document.getElementById("t_dashboard").style.display="none";
+		//document.getElementById("t_dashboard").style.display="none";
 		document.getElementById("my_leave").style.display="none";
+	}
+}
+	
+function f2(MyProfile){
+	if(MyProfile==0){
+		
+		document.getElementById("my_profile").style.display="none"; 
 		
 	}
+}	
 	
-	if(!MyProfile){
-		
-		document.getElementById("my_profile").style.display="none";
-		
-	}
-	
-	if(!ManagerAccess){
+function f3(ManagerAccess){
+	if(ManagerAccess==0){
 	
 		document.getElementById("d_progress").style.display="none";
 		document.getElementById("manage_task_dpt").style.display="none";
 		document.getElementById("d_leave").style.display="none";
 		
 	}
+}	
 	
-	if(!LeaderAccess){
+function f4(LeaderAccess){
+	
+	if(LeaderAccess==0){
 		
 		document.getElementById("manage_task_leader").style.display="none";
 		document.getElementById("t_leave").style.display="none";
 		
 	}
+}	
 	
-	if(!HRAccess){
-	
+function f5(HRAccess){
+	if(HRAccess==0){
+		
 		document.getElementById("emp_progress").style.display="none";
 		document.getElementById("manage_emp").style.display="none";
 		document.getElementById("emp_leave").style.display="none";
 	}
+}	
 	
-	if(!AdminAccess){
+function f6(AdminAccess){
+	if(AdminAccess==0){
 		
 		document.getElementById("manage_access").style.display="none";
 		
 	}
+}	
 	
-}
+	
+
