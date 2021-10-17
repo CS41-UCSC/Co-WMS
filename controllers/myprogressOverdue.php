a7143fd8a2e9c7421a21a1f617ad82541b9e65cd
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-10-17 13:23:57 +0530

    myprogress controller files & teamwork progress

---
+++ controllers/myprogressOverdue.php
@@ -0,0 +1,21 @@
+<?php
+
+class myprogressOverdue extends Controller{
+
+    public $msg;
+
+    function __construct()
+    {
+        parent::__construct();
+        session_start();
+    }
+
+    function index(){
+        
+        // $this->view->users =  $this->model->getData();
+        $this->view->render('myprogressOverdue');
+
+
+    }
+    
+}
---
+++ controllers/myprogressPending.php
@@ -0,0 +1,21 @@
+<?php
+
+class myprogressPending extends Controller{
+
+    public $msg;
+
+    function __construct()
+    {
+        parent::__construct();
+        session_start();
+    }
+
+    function index(){
+        
+        // $this->view->users =  $this->model->getData();
+        $this->view->render('myprogressPending');
+
+
+    }
+    
+}
