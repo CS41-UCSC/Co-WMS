0cbc5d8aed629ba700a132b85aa77c6e444b3788
AshaniImalsha <ashaniimalsha26@gmail.com>
2021-10-19 11:51:13 +0530

    connect with database

--- models/landingpage_Model.php
+++ models/landingpage_Model.php
@@ -10,9 +10,27 @@ class landingpage_Model extends Model{
 
     function loadData(){
 
-        
+        $sql1 ="SELECT COUNT(CASE WHEN task_assign.TaskStatus = 'InProgress' THEN 1 END) AS inprogress,
+                COUNT(CASE WHEN task_assign.TaskStatus = 'Completed' THEN 1 END) AS completed,
+                COUNT(CASE WHEN task_assign.TaskStatus = 'Approved' THEN 1 END) AS approved,
+                COUNT(CASE WHEN task_assign.TaskStatus = 'Pending' THEN 1 END) AS pending
+                FROM task_assign INNER JOIN task ON task_assign.TaskID = task.TaskID INNER JOIN team ON task.TeamID = team.TeamID 
+                INNER JOIN dept ON team.DeptID = dept.DeptID WHERE dept.Dept_Manager = '$_SESSION[login_user]' ";
+
+        return $this->db->runQuery($sql1);
 
     }
+
+    function loadHours(){
+
+        $sql2 = "SELECT task_assign.TaskStatus, SUM(task_assign.RequiredTime) AS totaltime FROM task_assign 
+        INNER JOIN task ON task_assign.TaskID = task.TaskID INNER JOIN team ON task.TeamID = team.TeamID 
+        INNER JOIN dept ON team.DeptID = dept.DeptID WHERE dept.Dept_Manager = '$_SESSION[login_user]' GROUP BY task_assign.TaskStatus ;";
+
+        return $this->db->runQuery($sql2);
+
+    }
+
 }
 
 
