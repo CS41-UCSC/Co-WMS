3f473ecb7bf0d7c311648f0627481c555037b86a
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-10-20 14:53:50 +0530

    manageDepartmentAdd

--- views/manageDepartmentAdd.php
+++ views/manageDepartmentAdd.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="manageDepartmentAdd.css">
+    <link rel="stylesheet" href="../Co-WMS/style/manageDepartmentAdd_style.css">
+    <link rel="stylesheet" href="../Co-WMS/style/nav_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
 </head>
@@ -14,7 +15,7 @@
         <button class="header-button" id="btnNav" type="button">
             <i class="fa fa-bars fa-lg"></i>
         </button>
-        <img src="logo.jpg" alt="" class="open-img">
+        <img src="../Co-WMS/Asserts/logo.jpg" alt="" class="open-img">
         <label for="" class="date"> <?php 
                                         $day;
                                         if(date("d") == 1){
@@ -45,26 +46,50 @@
                 <img src="../Asserts/logo.jpg" alt="" class="close-img">
             </div>-->
 
-            <a href="#" class="nav-link nav-link-active">
-                <i class="fa fa-user fa-lg"><span>Departmet Progress</span></i>
+            <a href="#" class="nav-link nav-link-active" id="manage_access">
+                <i class="fa fa-pencil-square-o fa-lg"><span>Manage Access</span></i>
             </a>
-            <a href="#" class="nav-link">
-                <i class="fa fa-bars fa-lg"><span>My Profile</span></i>
+            <a href="#" class="nav-link" id="dashboard">
+                <i class="fa fa-tachometer fa-lg" ><span>Dashboard</span></i>
             </a>
-            <a href="#" class="nav-link">
-                <i class="fa fa-bars fa-lg"><span>Manage Employee</span></i>
+			<a href="#" class="nav-link" id="d_progress">
+                <i class="fa fa-tachometer fa-lg" ><span>Department Progress</span></i>
             </a>
-            <a href="#" class="nav-link">
-                <i class="fa fa-bars fa-lg"><span>Manage Tasks</span></i>
+            <a href="#" class="nav-link" id="my_profile">
+                <i class="fa fa-user fa-lg" ><span>My Profile</span></i>
             </a>
-            <a href="#" class="nav-link">
-                <i class="fa fa-user fa-lg"><span>Employee Progress</span></i>
+			<a href="#" class="nav-link" id="my_progress">
+                <i class="fa fa-user fa-lg" ><span>My Progress</span></i>
             </a>
-            <a href="#" class="nav-link">
-                <i class="fa fa-bars fa-lg"><span>Employee Leave</span></i>
+			<a href="#" class="nav-link" id="t_progress">
+                <i class="fa fa-users fa-lg" ><span>Team Progress</span></i>
             </a>
-            <a href="#" class="nav-link">
-                <i class="fa fa-bars fa-lg"><span>Department Leave</span></i>
+			<a href="#" class="nav-link" id="emp_progress">
+                <i class="fa fa-users fa-lg" ><span>Employee Progress</span></i>
+            </a>
+            <a href="#" class="nav-link" id="manage_task_dpt">
+                <i class="fa fa-tasks fa-lg" ><span>Manage Tasks</span></i>
+            </a>
+			<a href="#" class="nav-link" id="manage_task_leader">
+                <i class="fa fa-tasks fa-lg" ><span>Manage Tasks</span></i>
+            </a>
+            <a href="#" class="nav-link" id="manage_emp">
+                <i class="fa fa-pencil-square-o fa-lg" ><span>Manage Employee</span></i>
+            </a>
+            <a href="#" class="nav-link" id="my_leave">
+                <i class="fa fa-list-alt fa-lg" ><span>My Leave</span></i>
+            </a>
+			<a href="#" class="nav-link" id="t_leave">
+                <i class="fa fa-list-alt fa-lg" ><span>Team Leave</span></i>
+            </a>
+			<a href="#" class="nav-link" id="d_leave">
+                <i class="fa fa-list-alt fa-lg" ><span>Department Leave</span></i>
+            </a>
+			<a href="#" class="nav-link" id="emp_leave">
+                <i class="fa fa-list-alt fa-lg" ><span>Employee Leave</span></i>
+            </a>
+			<a href="#" class="nav-link" id="logout">
+                <i class="fa fa-list-alt fa-lg" ><span>Logout</span></i>
             </a>
         </div> 
         <div class="nav-overlay"></div>   
@@ -86,47 +111,40 @@
     
     <main>
         <div class="container">
-            <div class="flex-container1">
-                <div class="text">All Departments</div>
-                <div>
-                    <form>
-                        <input type="text" id="search" name="Ename" placeholder="Search Department">
-                    </form>
+            <form action="" method="post">
+                <div class="flex-container1">
+                    <div>Add New Department</div>
+                   
                 </div>
-                <div>
-                        <form action="POST">
-                            <input type="submit" value="Add Department" class="rectan">
-                        </form>
+            </form>
+            
+                <div class="flex-container">
+                        <div>
+                            <label for="dname">Department Name :</label>
+                            <input type="text" id="dname" name="dname" placeholder="Department Name"><br>
+                        </div>
+                        <div>
+                            <label for="dId">Department Id :</label>
+                            <input type="text" id="dId" name="dId" placeholder="Department Id"><br>
+                        </div>
+                        <div>
+                            <label for="dMId">Department Manager Id :</label>
+                            <input type="text" id="dMId" name="dMId" placeholder="Department Manager Id"><br>
+                        </div>
+                        <div></div>
+                        
+                        <div>
+                            <form action="POST">
+                                <input type="submit" value="Cancel" class="rectan">
+                            </form>
+                        </div>
+                        <div>
+                            <form action="POST">
+                                <input type="submit" value="Add" class="rectan">
+                            </form>
+                        </div>
                 </div>
-            </div>
-            <div class="flex-container">
-                <!-- <div class="text">All Employees</div>  -->
-                
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th>Profile</th>
-                        <th>Department Id</th>
-                        <th>Department Name</th>
-			<th>Department Manager ID</th>
-                        <th>Edit</th>
-                        <th>Terminate</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td data-label="Profile"><a href="#"><i class="fa fa-user fa-2x" style="color:black;" aria-hidden="true"></i></a></td>
-                            <td data-label="Department Id">0001111</td>
-                            <td data-label="Department Name">Financial</td>
-			    <td data-label="Department Manager ID">2232</td>
-                            <td data-label="Edit"><a href="#"><i class="fa fa-pencil fa-2x" style="color:black;" aria-hidden="true"></i></a></td>
-                            <td data-label="Terminate"><a href="#"><i class="fa fa-minus-circle fa-2x" style="color:black;" aria-hidden="true"></i></a></td>
-                        </tr>
-
-                    </tbody>
-                </table>
-    
-            </div>
+           
         </div>
     </main>
 
