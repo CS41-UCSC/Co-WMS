0285595dd4c81f54ca14faf3d8b009d70af6e5f6
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2021-10-21 11:41:11 +0530

    inprogressSelect

---
+++ controllers/inprogressSelect.php
@@ -0,0 +1,21 @@
+<?php
+
+class inprogressSelect extends Controller{
+
+    public $msg;
+
+    function __construct()
+    {
+        parent::__construct();
+        session_start();
+    }
+
+    function index(){
+        
+        $this->view->users =  $this->model->getData();
+        $this->view->render('inprogressSelect');
+
+
+    }
+    
+}
