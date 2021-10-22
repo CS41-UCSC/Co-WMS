bbb0a883c7634fab2757433937ed8eda6b4221aa
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2021-10-22 09:21:07 +0530

    resetPass

---
+++ views/resetPass.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="resetPass_style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <div class="form">
+                <h1>Change <span>Password</span></h1>
+                <form action=POST>
+                    <div class="box">
+                        <label for="Email" class="new_pass">New Password</label><br>
+                        <input type="password" class="input"><br>
+                        <label for="Email" class="confirm_pass">Confirm New Password</label><br>
+                        <input type="password" class="input"><br>
+                    </div>
+                   
+                    
+
+                    <input type="submit" value="Change My Password" class="button">
+                    
+                </form>
+
+            </div>
+
+        </div>
+        <div class="right">
+            <div class="text1">
+                <h1>You Can Chanage The Password To Your Liking....</h1>
+            </div>
+            <div class="text2">
+                <h1>Password must have : </h1>
+                <ul>
+                    <li>Minimum 8 characters</li>
+                    <li>English uppercase/lowercase letters</li>
+                    <li>Numerals and Symbols</li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    
+</body>
+</html>
