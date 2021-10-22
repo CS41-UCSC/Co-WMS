da157206bc5df90bbdb3f8d19b858f3284587eff
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2021-10-22 18:10:09 +0530

    manageDepartmentAdd_Model

--- models/manageDepartmentAdd_Model.php
+++ models/manageDepartmentAdd_Model.php
@@ -6,11 +6,30 @@ class manageDepartmentAdd_Model extends Model{
     {
         parent::__construct();
     }
+    
+    function insertDepartment($dName,$dId,$dManagerId){
 
-    function getData(){
+        $sql = "INSERT INTO `dept`( `DeptID`, `DeptName`,`Dept_Manager`) VALUES ('$dId','$dName','$dManagerId') " ;
 
-        $sql = "SELECT * FROM dept";
-        return $this->db->runQuery($sql);
+
+        if($this->db->query($sql) == true){
+                $_SESSION['add-department'] = "yes";
+        }else{
+                $_SESSION['add-department'] = "no";
+        }
+
+    
+        // try{
+        //     $this->db->exec($sql);
+        //         $_SESSION['error-msg'] = "New record created successfully";
+        //     }
+        // catch(PDOException $e)
+        // {
+
+    	//     $_SESSION['error-msg'] = $e->getMessage();
+        // }
 
     }
+
+    
 }
