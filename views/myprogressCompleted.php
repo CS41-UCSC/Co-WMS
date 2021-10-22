80f9f95cdf73e583e5238f9ae2e81b90113d07a3
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-10-22 16:04:28 +0530

    myprogressCompleted Update

--- views/myprogressCompleted.php
+++ views/myprogressCompleted.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Co-WMS/style/myprogressCompleted_style.css">
-    <link rel="stylesheet" href="../Co-WMS/style/nav_style.css">
+    <link rel="stylesheet" href="../Co-WMS/style/navbar_style.css">
+    <script language="javascript" src="../Co-WMS/views/navigation.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
 </head>
 
-<body class="preload">
+<body class="preload" onload='setbutton("<?php echo $_SESSION["memberaccess"] ?>","<?php echo $_SESSION["myprofile"] ?>","<?php echo $_SESSION["manageraccess"] ?>","<?php echo $_SESSION["leaderaccess"] ?>","<?php echo $_SESSION["hraccess"] ?>","<?php echo $_SESSION["adminaccess"] ?>")'>
     <header class="header">
         <button class="header-button" id="btnNav" type="button">
             <i class="fa fa-bars fa-lg"></i>
@@ -33,6 +34,8 @@
                                     ?>
         </label>
         <div class="notification"><a href="#" ><i class="fa fa-bell fa-lg "></i></a></div>
+        <span class="user-login"><?php echo $_SESSION['login_user'] ?></span>
+        <img  class="img-rounded-circle" src="../Co-WMS/Asserts/<?php if($result[0]['Userimg']) {echo $result[0]['Userimg'];} else {echo 'avator.jpg';} ?>" alt="">
 
     
 
@@ -49,19 +52,19 @@
             <a href="#" class="nav-link nav-link-active" id="manage_access">
                 <i class="fa fa-pencil-square-o fa-lg"><span>Manage Access</span></i>
             </a>
-            <a href="#" class="nav-link" id="dashboard">
+            <a href="landingpage" class="nav-link" id="dashboard">
                 <i class="fa fa-tachometer fa-lg" ><span>Dashboard</span></i>
             </a>
 			<a href="#" class="nav-link" id="d_progress">
                 <i class="fa fa-tachometer fa-lg" ><span>Department Progress</span></i>
             </a>
-            <a href="#" class="nav-link" id="my_profile">
+            <a href="myprofile" class="nav-link" id="my_profile">
                 <i class="fa fa-user fa-lg" ><span>My Profile</span></i>
             </a>
-			<a href="#" class="nav-link" id="my_progress">
+			<a href="myprogressAccepted" class="nav-link" id="my_progress">
                 <i class="fa fa-user fa-lg" ><span>My Progress</span></i>
             </a>
-			<a href="#" class="nav-link" id="t_progress">
+			<a href="teamWorkProgress" class="nav-link" id="t_progress">
                 <i class="fa fa-users fa-lg" ><span>Team Progress</span></i>
             </a>
 			<a href="#" class="nav-link" id="emp_progress">
@@ -88,7 +91,7 @@
 			<a href="#" class="nav-link" id="emp_leave">
                 <i class="fa fa-list-alt fa-lg" ><span>Employee Leave</span></i>
             </a>
-			<a href="#" class="nav-link" id="logout">
+			<a href="homepage" class="nav-link" id="logout">
                 <i class="fa fa-list-alt fa-lg" ><span>Logout</span></i>
             </a>
         </div> 
@@ -98,10 +101,10 @@
     <nav>
     <input id="nav-toggle" type="checkbox"> 
         <ul class="links">
-            <li><a href="#Completed">Completed</a></li>
-            <li><a href="#In Progress">In Progress</a></li>
-            <li><a href="#Overdue">Overdue</a></li>
-            <li><a href="#Pending">Pending</a></li>
+            <li><a href="myprogressCompleted">Completed</a></li>
+            <li><a href="myprogressAccepted">In Progress</a></li>
+            <li><a href="myprogressOverdue">Overdue</a></li>
+            <li><a href="myprogressPending">Pending</a></li>
         </ul>
         <label for="nav-toggle" class="icon-burger">
             <div class="line"></div>
@@ -176,10 +179,10 @@
         </div>
     </main>
 
-    <footer class="footer">
+    <!-- <footer class="footer">
         <label for="" class="footer-data">© 2021, All rights reserved by CO - WMS <br>
                         No: 23, Flower Avenue, Colombo 7, Sri Lanka.</label>
-    </footer>
+    </footer> -->
     <script>
         document.addEventListener("DOMContentLoaded", () =>{
             const nav = document.querySelector(".nav");
