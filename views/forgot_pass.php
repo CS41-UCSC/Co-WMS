5a6b8d3c5c788d8d304668dc2200c96b0eec55f6
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2021-10-22 09:19:24 +0530

    forgot_pass

---
+++ views/forgot_pass.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="forgot_pass_style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <div class="form">
+                <h1>Forgot <span>Password</span></h1>
+                <form action=POST>
+                    <label for="Email" class="email">Username</label><br>
+                    <input type="username" class="input"><br>
+
+                    <input type="submit" value="Send" class="button">
+                    <h4>Or</h4>
+
+                    <div class="link">
+                        <h4>Back to <a href="">Login</a></h4>
+                    </div>
+                </form>
+
+            </div>
+
+        </div>
+        <div class="right">
+            <div class="text1">
+                <h1>Do you not Remember the Password?</h1>
+            </div>
+            <div class="text2">
+                <h1>No Problem !!! Enter your Email or Username. We will send you an Email with instruction to reset your password......</h1>
+            </div>
+
+        </div>
+    </div>
+    
+</body>
+</html>
