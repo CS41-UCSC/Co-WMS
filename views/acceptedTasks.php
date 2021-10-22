13f1ea831792c8cb60f63290ed022c588c1b805c
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2021-10-22 15:35:01 +0530

    acceptedTasks

--- views/acceptedTasks.php
+++ views/acceptedTasks.php
@@ -4,22 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Co-WMS/acceptedTasks_style.css">
-    <link rel="stylesheet" href="../Co-WMS/nav_style.css">
-    <!-- <script language="javascript" src="navigation.js"> -->
+    <link rel="stylesheet" href="../Co-WMS/style/acceptedTasks_style.css">
+    <link rel="stylesheet" href="../Co-WMS/style/nav_style.css">
+    <script language="javascript" src="navigation.js">
+    <script language="javascript" src="../Co-WMS/views/navigation.js"></script>
 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
 </head>
 
-<body class="preload">
+<body class="preload" onload='setbutton("<?php echo $_SESSION["memberaccess"] ?>","<?php echo $_SESSION["myprofile"] ?>","<?php echo $_SESSION["manageraccess"] ?>","<?php echo $_SESSION["leaderaccess"] ?>","<?php echo $_SESSION["hraccess"] ?>","<?php echo $_SESSION["adminaccess"] ?>")'>
     <header class="header">
         <button class="header-button" id="btnNav" type="button">
             <i class="fa fa-bars fa-lg"></i>
         </button>
-        <img src="logo.jpeg" alt="" class="open-img">
-        <label for="" class="date"> <?php 
+        <img src="../Co-WMS/Asserts/logo.jpg" alt="" class="open-img">
+        <label for="" class="date"> 
+                                    <?php 
                                         $day;
                                         if(date("d") == 1){
                                             $day = "st ";
@@ -36,9 +38,8 @@
                                     ?>
         </label>
         <div class="notification"><a href="#" ><i class="fa fa-bell fa-lg "></i></a></div>
-        <!-- <span class="user-login"><?php echo $_SESSION['login_user'] ?></span> -->
-        <img class="img-rounded-circle" src="../Co-WMS/Asserts/avator.jpg" alt="">
-
+        <span class="user-login"><?php echo $_SESSION['login_user'] ?></span>
+        <img  class="img-rounded-circle" src="../Co-WMS/Asserts/<?php if($result[0]['Userimg']) {echo $result[0]['Userimg'];} else {echo 'avator.jpg';} ?>" alt="">
     
 
     </header>
@@ -54,16 +55,17 @@
             <a href="#" class="nav-link nav-link-active" id="manage_access">
                 <i class="fa fa-pencil-square-o fa-lg"><span>Manage Access</span></i>
             </a>
-            <a href="#" class="nav-link" id="dashboard">
+            <a href="landingpage" class="nav-link" id="dashboard">
                 <i class="fa fa-tachometer fa-lg" ><span>Dashboard</span></i>
             </a>
 			<a href="#" class="nav-link" id="d_progress">
                 <i class="fa fa-tachometer fa-lg" ><span>Department Progress</span></i>
             </a>
-            <a href="#" class="nav-link" id="my_profile">
+            <a href="myprofile" class="nav-link" id="my_profile">
                 <i class="fa fa-user fa-lg" ><span>My Profile</span></i>
             </a>
-			<a href="#" class="nav-link" id="my_progress">
+			<a href="completedTasks
+            " class="nav-link" id="my_progress">
                 <i class="fa fa-user fa-lg" ><span>My Progress</span></i>
             </a>
 			<a href="#" class="nav-link" id="t_progress">
@@ -93,7 +95,7 @@
 			<a href="#" class="nav-link" id="emp_leave">
                 <i class="fa fa-list-alt fa-lg" ><span>Employee Leave</span></i>
             </a>
-			<a href="#" class="nav-link" id="logout">
+			<a href="homepage" class="nav-link" id="logout">
                 <i class="fa fa-list-alt fa-lg" ><span>Logout</span></i>
             </a>
         </div> 
@@ -103,10 +105,10 @@
     <nav>
     <input id="nav-toggle" type="checkbox"> 
         <ul class="links">
-            <li><a href="#Completed">Completed</a></li>
-            <li><a href="#In Progress">In Progress</a></li>
-            <li><a href="#Overdue">Overdue</a></li>
-            <li><a href="#Pending">Pending</a></li>
+            <li><a href="completedTasks">Completed</a></li>
+            <li><a href="acceptedTasks">In Progress</a></li>
+            <li><a href="overdueTasks">Overdue</a></li>
+            <li><a href="pendingTasks">Pending</a></li>
         </ul>
         <label for="nav-toggle" class="icon-burger">
             <div class="line"></div>
