b4ac29c25bf497bd4be6d2ceb0e969605a4dd74e
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-10-22 17:02:39 +0530

    manageTeamAdd Update

--- controllers/manageTeamAdd.php
+++ controllers/manageTeamAdd.php
@@ -11,10 +11,24 @@ class manageTeamAdd extends Controller{
     }
 
     function index(){
-        
-        // $this->view->users =  $this->model->getData();
+        $this->view->team=0;
+
         $this->view->render('manageTeamAdd');
 
+
+    }
+
+    function setData(){
+        
+      
+        $teamName= $_POST['tname'];
+        $DeptId = $_POST['dId'];
+
+        // $hash = password_hash($password, PASSWORD_DEFAULT);
+
+        $this->model->insertTeam($teamName,$DeptId);
+
+        header('location: http://localhost/CO-WMS/manageTeamAdd');
 
     }
     
