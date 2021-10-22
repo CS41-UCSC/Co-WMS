361ebde5eaed6d6cab5619ccdd1e6083ed774c39
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-10-22 16:08:55 +0530

    teamWorkProgress Update

--- views/teamWorkProgress.php
+++ views/teamWorkProgress.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Co-WMS/style/teamWorkProgress_style.css">
-    <link rel="stylesheet" href="../Co-WMS/style/nav_style.css">
+    <link rel="stylesheet" href="../Co-WMS/style/navbar_style.css">
+    <script language="javascript" src="../Co-WMS/views/navigation.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
 </head>
 
-<body class="preload">
+<body class="preload" onload='setbutton("<?php echo $_SESSION["memberaccess"] ?>","<?php echo $_SESSION["myprofile"] ?>","<?php echo $_SESSION["manageraccess"] ?>","<?php echo $_SESSION["leaderaccess"] ?>","<?php echo $_SESSION["hraccess"] ?>","<?php echo $_SESSION["adminaccess"] ?>")'>
     <header class="header">
         <button class="header-button" id="btnNav" type="button">
             <i class="fa fa-bars fa-lg"></i>
@@ -33,6 +34,8 @@
                                     ?>
         </label>
         <div class="notification"><a href="#" ><i class="fa fa-bell fa-lg "></i></a></div>
+        <span class="user-login"><?php echo $_SESSION['login_user'] ?></span>
+        <img  class="img-rounded-circle" src="../Co-WMS/Asserts/<?php if($result[0]['Userimg']) {echo $result[0]['Userimg'];} else {echo 'avator.jpg';} ?>" alt="">
 
     
 
@@ -49,19 +52,19 @@
             <a href="#" class="nav-link nav-link-active" id="manage_access">
                 <i class="fa fa-pencil-square-o fa-lg"><span>Manage Access</span></i>
             </a>
-            <a href="#" class="nav-link" id="dashboard">
+            <a href="landingpage" class="nav-link" id="dashboard">
                 <i class="fa fa-tachometer fa-lg" ><span>Dashboard</span></i>
             </a>
 			<a href="#" class="nav-link" id="d_progress">
                 <i class="fa fa-tachometer fa-lg" ><span>Department Progress</span></i>
             </a>
-            <a href="#" class="nav-link" id="my_profile">
+            <a href="myprofile" class="nav-link" id="my_profile">
                 <i class="fa fa-user fa-lg" ><span>My Profile</span></i>
             </a>
-			<a href="#" class="nav-link" id="my_progress">
+			<a href="myprogressAccepted" class="nav-link" id="my_progress">
                 <i class="fa fa-user fa-lg" ><span>My Progress</span></i>
             </a>
-			<a href="#" class="nav-link" id="t_progress">
+			<a href="teamWorkProgress" class="nav-link" id="t_progress">
                 <i class="fa fa-users fa-lg" ><span>Team Progress</span></i>
             </a>
 			<a href="#" class="nav-link" id="emp_progress">
@@ -88,14 +91,14 @@
 			<a href="#" class="nav-link" id="emp_leave">
                 <i class="fa fa-list-alt fa-lg" ><span>Employee Leave</span></i>
             </a>
-			<a href="#" class="nav-link" id="logout">
+			<a href="homepage" class="nav-link" id="logout">
                 <i class="fa fa-list-alt fa-lg" ><span>Logout</span></i>
             </a>
         </div> 
         <div class="nav-overlay"></div>   
     </nav>
                             
-    <nav>
+    <!-- <nav>
     <input id="nav-toggle" type="checkbox"> 
         <ul class="links">
             <li><a href="#Employees">Employees</a></li>
@@ -107,7 +110,7 @@
             <div class="line"></div>
             <div class="line"></div>
         </label>
-    </nav>
+    </nav> -->
     
     <main>
         <div class="container">
@@ -129,7 +132,7 @@
                             <td data-label="Profile"><a href="#"><i class="fa fa-user fa-2x" style="color:black;" aria-hidden="true"></i></a></td>
                             <td data-label="Employee Id">0001111</td>
                             <td data-label="Employee Name">Navod Kamilka</td>
-                            <td data-label="View Work Progress"><a href="#"><i class="fa fa-eye fa-2x" style="color:black;" aria-hidden="true"></i></a></td>
+                            <td data-label="View Work Progress"><a href="landingpage"><i class="fa fa-eye fa-2x" style="color:black;" aria-hidden="true"></i></a></td>
                         </tr>
                         <tr>
                             <td data-label="Profile"><a href="#"><i class="fa fa-user fa-2x" style="color:black;" aria-hidden="true"></i></a></td>
