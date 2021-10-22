95f3336a9cccb74feaf750040bc6b7223d796c53
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2021-10-22 18:08:46 +0530

    manageDepartmentAdd

--- controllers/manageDepartmentAdd.php
+++ controllers/manageDepartmentAdd.php
@@ -12,9 +12,20 @@ class manageDepartmentAdd extends Controller{
 
     function index(){
         
+        $this->view->department = 0;
+        
         // $this->view->users =  $this->model->getData();
         $this->view->render('manageDepartmentAdd');
 
+
+    }
+    function setData(){
+        $dName = $_POST['dname'];
+        $dId = $_POST['dId'];
+        $dManagerId = $_POST['dMId'];
+        $this->model->insertDepartment($dName,$dId,$dManagerId);
+
+        header('location: http://localhost/CO-WMS/manageDepartmentAdd');
 
     }
     
