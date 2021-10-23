595b806df644a384dd67a714a3f0049f5ad2a3a8
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-10-23 20:11:38 +0530

    myprogress files

---
+++ controllers/myprogressAccepted.php
@@ -0,0 +1,21 @@
+<?php
+
+class myprogressAccepted extends Controller{
+
+    public $msg;
+
+    function __construct()
+    {
+        parent::__construct();
+        session_start();
+    }
+
+    function index(){
+        
+        // $this->view->users =  $this->model->getData();
+        $this->view->render('myprogressAccepted');
+
+
+    }
+    
+}
