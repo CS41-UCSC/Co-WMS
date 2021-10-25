ad3611436de95d9e3c68e950f0c7f5746f5520d0
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2021-10-25 17:16:14 +0530

    forgot/reset password

---
+++ views/forgotPassword.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Co-WMS/style/forgotPassword_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <div class="form">
+                <h1>Forgot <span>Password</span></h1>
+                <form action=POST>
+                    <label for="Email" class="email">Username</label><br>
+                    <input type="username" class="input"><br>
+
+                    <input type="submit" value="Send" class="button">
+                    <h4>Or</h4>
+
+                    <div class="link">
+                        <h4>Back to <a href="">Login</a></h4>
+                    </div>
+                </form>
+
+            </div>
+
+        </div>
+        <div class="right">
+            <div class="text1">
+                <h1>Do you not Remember the Password?</h1>
+            </div>
+            <div class="text2">
+                <h1>No Problem !!! Enter your Email or Username. We will send you an Email with instruction to reset your password......</h1>
+            </div>
+
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ views/resetPassword.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Co-WMS/style/resetPassword_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <div class="form">
+                <h1>Change <span>Password</span></h1>
+                <form action=POST>
+                    <div class="box">
+                        <label for="Email" class="new_pass">New Password</label><br>
+                        <input type="password" class="input"><br>
+                        <label for="Email" class="confirm_pass">Confirm New Password</label><br>
+                        <input type="password" class="input"><br>
+                    </div>
+                   
+                    
+
+                    <input type="submit" value="Change My Password" class="button">
+                    
+                </form>
+
+            </div>
+
+        </div>
+        <div class="right">
+            <div class="text1">
+                <h1>You Can Chanage The Password To Your Liking....</h1>
+            </div>
+            <div class="text2">
+                <h1>Password must have : </h1>
+                <ul>
+                    <li>Minimum 8 characters</li>
+                    <li>English uppercase/lowercase letters</li>
+                    <li>Numerals and Symbols</li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    
+</body>
+</html>
