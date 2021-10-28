ccdd411a3a9163d935ef58e5c44f158b17c24750
mtbstn <thushibstn24@gmail.com>
2021-10-29 00:51:08 +0530

    model edit

--- models/leave_Model.php
+++ models/leave_Model.php
@@ -12,7 +12,25 @@ class leave_Model extends Model{
 	}
 	
 	function insert($start, $end, $leavetype){
-		$sql="INSERT INTO empleave (StartDate,EndDate,LeaveType) VALUES ('$start','$end','$leavetype')";
+		$empID = $_SESSION['login_user'];
+		$status = "pending";
+		$sql="INSERT INTO leaves (StartDate,EndDate,Reason,EmpID,status) VALUES ('$start','$end','$leavetype','$empID',)";
+		return $this->db->runQuery($sql);
+		
+		/*try{
+            $this->db->exec($sql);
+                $_SESSION['error-msg'] = "New record created successfully";
+            }
+        catch(PDOException $e)
+        {
+
+    	    $_SESSION['error-msg'] = $e->getMessage();
+        }*/
+	
+	}
+	
+	function delete($start, $end, $leavetype){
+		/*$sql="INSERT INTO empleave (StartDate,EndDate,Reason,EmpID) VALUES ('$start','$end','$leavetype','$_SESSION['login_user']')";
 		return $this->db->runQuery($sql);
 		echo "<script>alert('Requested Succefully')</script>";
 		
