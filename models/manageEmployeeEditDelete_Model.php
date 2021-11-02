dd3154d71cf4aabcdd090da0647a9ff6b988c6dd
NavodKamilka <62130202+NavodKamilka@users.noreply.github.com>
2021-11-02 07:49:57 +0530

    update

--- models/manageEmployeeEditDelete_Model.php
+++ models/manageEmployeeEditDelete_Model.php
@@ -7,9 +7,9 @@ class manageEmployeeEditDelete_Model extends Model{
         parent::__construct();
     }
 
-    function editEmployee($empid,$empname,$empemail,$emprole,$empstatus){
+    function editEmployee($empid,$empname,$empemail,$emprole){
 
-        $sql = "UPDATE systemuser SET EmpName='$empname' , EmpEmail='$empemail' , EmpRole='$emprole' , EmpStatus='$empstatus' WHERE EmpID=('$empid') " ;
+        $sql = "UPDATE systemuser SET EmpName='$empname' , EmpEmail='$empemail' , EmpRole='$emprole' WHERE EmpID=('$empid') " ;
 
         // return $this->db->query($sql);
 
